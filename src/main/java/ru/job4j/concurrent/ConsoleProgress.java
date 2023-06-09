218e96cb2e887e3d46e82ac54b673fc3217e88d2
Arthur Magomedov <devmagomedov@gmail.com>
2023-06-09 13:16:47 +0300

    6. Прерывание блокированной нити. [#267413 #421929]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -21,7 +21,7 @@ public class ConsoleProgress implements Runnable {
                 }
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
