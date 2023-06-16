ff339c35ce619eec0d21123bf0f5e4775ba86fe5
Arthur Magomedov <devmagomedov@gmail.com>
2023-06-16 14:25:47 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #423372]

--- src/main/java/ru/job4j/io/GetContent.java
+++ src/main/java/ru/job4j/io/GetContent.java
@@ -7,17 +7,17 @@ public class GetContent implements GetContentStrategy {
 
     @Override
     public String getContent(Predicate<Character> filter, File file) {
-        String output = "";
+        StringBuilder output = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             int data;
-            while ((data = in.read()) > 0) {
+            while ((data = in.read()) != -1) {
                 if (filter.test((char) data)) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return output;
+        return String.valueOf(output);
     }
 }
