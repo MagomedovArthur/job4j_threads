8115ffa98494acb37a791deda1aef37052d5f9f1
Arthur Magomedov <devmagomedov@gmail.com>
2023-06-16 17:32:41 +0300

    8. Скачивание файла с ограничением. [#144271 #421944]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -3,25 +3,41 @@ package ru.job4j.thread;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class Wget implements Runnable {
     private final String url;
+    private final String fileName;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[speed];
             int bytesRead;
+            int downloadData = 0;
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+                Calendar startDate = Calendar.getInstance();
+                long startSeconds = TimeUnit.MILLISECONDS.toSeconds(startDate.getTimeInMillis());
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
+                downloadData = bytesRead;
+                if (downloadData >= speed) {
+                    Calendar currentDate = Calendar.getInstance();
+                    long currentSeconds = TimeUnit.MILLISECONDS.toSeconds(currentDate.getTimeInMillis());
+                    long interval = currentSeconds - startSeconds;
+                    if (interval < 1) {
+                        Thread.sleep(1000 - TimeUnit.MILLISECONDS.toMillis(interval));
+                        downloadData = 0;
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,7 +48,7 @@ public class Wget implements Runnable {
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         validateArgs(args);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, speed, "10Mb.dat"));
         wget.start();
         wget.join();
     }
@@ -41,7 +57,7 @@ public class Wget implements Runnable {
         if (args[0].isEmpty()) {
             throw new IllegalArgumentException("URL string is empty!");
         }
-        if (Integer.parseInt(args[1]) > 1024) {
+        if (Integer.parseInt(args[1]) != 0) {
             throw new IllegalArgumentException();
         }
     }
