206bf04b61daa5a228d83bfb85f50860fe26aced
Arthur Magomedov <devmagomedov@gmail.com>
2023-06-22 15:43:50 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,22 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger count = new AtomicInteger();
+
+    public void increment() {
+        count.incrementAndGet();
+    }
+
+    public int get() {
+        if (count == null) {
+            throw new UnsupportedOperationException("Count is not impl.");
+        }
+        return count.get();
+    }
+}
