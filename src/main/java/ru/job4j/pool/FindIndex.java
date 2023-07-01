4b2742a5e0b5e953c30ede70fb46a2da7af18f2f
Arthur Magomedov <devmagomedov@gmail.com>
2023-07-01 17:36:13 +0300

    3. ForkJoinPool [#315067 #425356]

--- src/main/java/ru/job4j/pool/FindIndex.java
+++ src/main/java/ru/job4j/pool/FindIndex.java
@@ -1,21 +1,25 @@
 package ru.job4j.pool;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class FindIndex<T> extends RecursiveTask<Integer> {
-
     private final T[] array;
     private final T object;
+    private final int from;
+    private final int to;
 
-    public FindIndex(T[] array, T object) {
+    public FindIndex(T[] array, T object, int from, int to) {
         this.array = array;
         this.object = object;
+        this.from = from;
+        this.to = to;
     }
 
-    private int findIndex(T[] array, T object) {
+    private int findIndex(T[] array, T object, int from, int to) {
         int result = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(object)) {
+        for (int i = from; i < to; i++) {
+            if (object.equals(array[i])) {
                 result = i;
                 break;
             }
@@ -25,11 +29,20 @@ public class FindIndex<T> extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-        if (array.length <= 10) {
-            return findIndex(array, object);
+        if (to - from <= 10) {
+            return findIndex(array, object, from, to);
         }
-        FindIndex findIndex = new FindIndex(array, object);
-        findIndex.fork();
-        return findIndex(array, object);
+        int middle = (from + to) / 2;
+        FindIndex<T> findIndexRight = new FindIndex<>(array, object, from, middle);
+        FindIndex<T> findIndexLeft = new FindIndex<>(array, object, middle + 1, to);
+        findIndexRight.fork();
+        findIndexLeft.fork();
+        Integer right = findIndexRight.join();
+        Integer left = findIndexLeft.join();
+        return Math.max(right, left);
+    }
+
+    public static <T> int search(T[] array, T object) {
+        return new ForkJoinPool().invoke(new FindIndex<>(array, object, 0, array.length - 1));
     }
 }
--- src/test/java/ru/job4j/pool/FindIndexTest.java
+++ src/test/java/ru/job4j/pool/FindIndexTest.java
@@ -12,7 +12,7 @@ class FindIndexTest {
     void whenElementNotFound() {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         String[] array = new String[]{"asdas", "iwquy", "iopip", "asd", "asdas", "iwquy", "iopip"};
-        int result = forkJoinPool.invoke(new FindIndex<>(array, "artur"));
+        int result = forkJoinPool.invoke(new FindIndex<>(array, "artur", 0, array.length - 1));
         assertThat(result).isEqualTo(-1);
     }
 
@@ -20,17 +20,15 @@ class FindIndexTest {
     void whenDifferentDataTypes() {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         String[] array = new String[]{"asdas", "iwquy", "iopip", "asd", "asdas", "iwquy", "iopip"};
-        int result = forkJoinPool.invoke(new FindIndex<>(array, 324));
+        int result = forkJoinPool.invoke(new FindIndex<>(array, 324, 0, array.length - 1));
         assertThat(result).isEqualTo(-1);
     }
 
     @Test
     void whenElementIsFound() {
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
         String[] array = new String[]{"asdas", "iwquy", "iopip", "asd", "asdas", "iwquy", "iopip",
-                "asdas", "iwquy", "iopip", "asd", "asdas", "iwquy", "iopip",
-                "asdas", "iwquy", "iopip", "asd", "asdas", "iwquy", "iopip"};
-        int result = forkJoinPool.invoke(new FindIndex<>(array, "asd"));
-        assertThat(result).isEqualTo(3);
+                "asdas", "iwquy", "iopip", "asdas", "iwquy", "iopip",
+                "asdas", "iwquy", "iopip", "asdas", "iwquy", "iopip"};
+        assertThat(FindIndex.search(array, "asd")).isEqualTo(3);
     }
 }
